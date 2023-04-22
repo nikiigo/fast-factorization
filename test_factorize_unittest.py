a96e562f7ea797251b76b68a1ce1703fb077e112
Igor.Nikitin.-ND <igor.nikitin.-nd@disney.com>
2023-04-22 06:45:18 -0600

    Initial commit

---
+++ test_factorize_unittest.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+class TestFactorize(unittest.TestCase):
+
+    def test_digit_root(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_last_digits(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_is_even(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_is_perfect_square(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_factorize(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
